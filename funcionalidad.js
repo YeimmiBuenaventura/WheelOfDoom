e821851de7dfb04129320f264e7c979ac5c29387
Karen Andrea Niño Angulo <107644961+Andrea-25-11@users.noreply.github.com>
2022-11-25 11:47:56 -0500

    Aplicando estilos y js

--- funcionalidad.js
+++ funcionalidad.js
@@ -8,18 +8,19 @@
 // --------------------JAVASCRIPT JUEGO.HTML--------------------
 
 //CÓDIGO ANIMACIÓN MUÑEQUITO
-var contexto;
+        var contexto;
         var imagenes = [];
         var pos = 0;
+
         function iniciar (){
-            var canvas = document.getElementById("lienzo")
+            var canvas = document.getElementById("jugador");
             canvas.width = 1200;
-            canvas.height = 1500;
+            canvas.height = 1200;
             contexto = canvas.getContext("2d");
             for(i=0; i < 6; ++i)
             {
                 imagenes [i] = new Image();
-                imagenes [i].src = "muñeco/"+(i+1) +".png";
+                imagenes [i].src = "imagenes/ilustraciones/victim/"+(i+1) +".png";
             }
             setInterval(animar, 300 );
         }
@@ -28,5 +29,26 @@ var contexto;
             ++pos; if(pos == 6) pos = 0;
         }
 //FINALIZACIÓN ANIMACIÓN MUÑEQUITO
+// CÓDIGO ANIMACIÓN KILLER
+        var contexto;
+        var imagenes = [];
+        var pos = 0;
+
+        function asesino (){
+            var canvas = document.getElementById("matador");
+            canvas.width = 1200;
+            canvas.height = 1200;
+            contexto = canvas.getContext("2d");
+            for(i=0; i < 6; ++i)
+            {
+                imagenes [i] = new Image();
+                imagenes [i].src = "imagenes/ilustraciones/victim/"+(i+1) +".png";
+            }
+            setInterval(animar, 300 );
+        }
+        function animar(){
+            contexto.drawImage(imagenes[pos], 0, 0);
+            ++pos; if(pos == 6) pos = 0;
+        }
 
 //--------------------JAVASCRIPT GAME OVER--------------------
