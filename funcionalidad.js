2ab23ec274dfa2c7eefcfddd79d6cdcd75dd19ae
fredybernal <jhonbernal542@gmail.com>
2022-11-28 16:21:58 -0500

    animacion para celular

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,3 +1,4 @@
+/*
 var contexto;
 var imagenes = [];
 var pos2 = 0;
@@ -42,6 +43,56 @@ function animar2() {
     ++pos2; if (pos2 == 2) pos2 = 0;
 
 }
+*/
+
+// para celular 
+
+var contexto;
+var imagenes = [];
+var pos2 = 0;
+var contexto2;
+var imagenes2 = [];
+var pos = 0;
+
+function iniciar() {
+    var canvas = document.getElementById("lienzo");
+
+    canvas.width = 1200;
+    canvas.height = 900;
+
+    contexto = canvas.getContext("2d");
+
+    for (i = 0; i < 6; ++i) {
+        imagenes[i] = new Image();
+        imagenes[i].src = "./muñeco para celular/" + (i + 1) + ".png";
+    }
+
+    setInterval(animar, 500);
+
+    contexto2 = canvas.getContext("2d")
+
+    for (i = 0; i < 2; ++i) {
+        imagenes2[i] = new Image();
+        imagenes2[i].src = "./rojo para celular/" + (i + 1) + ".png";
+    }
+    
+    setInterval(animar2, 500);
+    
+}
+
+function animar() {
+    contexto.drawImage(imagenes[pos], 500, 20);
+    ++pos; if (pos == 6) pos = 0;
+
+}
+
+function animar2() {
+    contexto.drawImage(imagenes2[pos2], 500, 400);
+    ++pos2; if (pos2 == 2) pos2 = 0;
+
+}
+
+
 
 
 
