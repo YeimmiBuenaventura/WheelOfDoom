73f0734dd0898af6fa55de9d5b1ebeddaa6a677e
Karen Andrea Niño Angulo <107644961+Andrea-25-11@users.noreply.github.com>
2022-11-28 15:57:18 -0500

    Modificando js

--- funcionalidad.js
+++ funcionalidad.js
@@ -8,47 +8,7 @@
 // --------------------JAVASCRIPT JUEGO.HTML--------------------
 
 //CÓDIGO ANIMACIÓN MUÑEQUITO
-        var contexto;
-        var imagenes = [];
-        var pos = 0;
-
-        function iniciar (){
-            var canvas = document.getElementById("jugador");
-            canvas.width = 1200;
-            canvas.height = 1200;
-            contexto = canvas.getContext("2d");
-            for(i=0; i < 6; ++i)
-            {
-                imagenes [i] = new Image();
-                imagenes [i].src = "imagenes/ilustraciones/victim/"+(i+1) +".png";
-            }
-            setInterval(animar, 300 );
-        }
-        function animar(){
-            contexto.drawImage(imagenes[pos], 0, 0);
-            ++pos; if(pos == 6) pos = 0;
-        }
 //FINALIZACIÓN ANIMACIÓN MUÑEQUITO
 // CÓDIGO ANIMACIÓN KILLER
-        var contexto;
-        var imagenesKiller = [];
-        var pos = 0;
-
-        function disparo (){
-            var canvas = document.getElementById("matador");
-            canvas.width = 1200;
-            canvas.height = 1200;
-            contexto = canvas.getContext("2d");
-            for(i=0; i < 3; ++i)
-            {
-                imagenesKiller [i] = new Image();
-                imagenesKiller [i].src = "imagenes/ilustraciones/killerCel"+(i+1) +".png";
-            }
-            setInterval(animar2, 300 );
-        }
-        function animar2(){
-            contexto.drawImage(imagenesKiller[pos], 0, 0);
-            ++pos; if(pos == 3) pos = 0;
-        }
 
 //--------------------JAVASCRIPT GAME OVER--------------------
