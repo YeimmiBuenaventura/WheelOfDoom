4f690a759482c402365323e3cf06b2fdb58b1b63
Juliana1497 <cristanchojuli@gmail.com>
2022-11-28 15:50:43 -0500

    agrgando comentarios

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,6 +1,6 @@
-let add = document.getElementById('add');
-let player2 = document.getElementById('letra-2')
-let players = [];
+let add = document.getElementById('add');//Se llama el id add del html para ponerle funcionalidad al icono de agregar
+let player2 = document.getElementById('letra-2')//Se llama el id del input en html para poder agregarle la funcionalidad correspondiente
+let players = [];//Se declara un array donde se guardaran los jugadores que se introduzcan
 
 
 add.addEventListener("click", (e) => {
@@ -26,20 +26,21 @@ add.addEventListener("click", (e) => {
     guardarEnStorage(players);
 });
   
-const render = () => {
-  let htmlPlayers = ``;
+const render = () => {//Se declara una variable constante como objeto donde se busca crear nuevos objetos en el html a partir de javaScript para que aparezcan solo cuando sea pertinente 
+  let htmlPlayers = ``;//Se crea una nueva variable de tipo string vacia donde se sumara cada objeto a la lista a medida que los vaya agregando el usuario
    
-  let list = document.getElementById("list");
+  let list = document.getElementById("list");//Se llama desde el html el id list para indicarle donde se guardaran en el html los objetos creados desde javaScript
   
-  players.forEach(
-    (player) =>
+  players.forEach(//Se usa el metodo for each para recorrer todos los elementos del array
+    (player) =>/* Se crea un objeto de tipo lista la cual contendra el nombre de cada jugador ingresado, agregando un icono 
+    para eliminar el nombre del jugador de la lista cuando lo desee el usuario*/
       (htmlPlayers += `<li class="itemList" >
         <p class="playersName">${player.playerName}</p>
         <i class="bi bi-trash3-fill delete"id="${player.id}"></i>
         </li>`)
   );
-  list.innerHTML = htmlPlayers;
-  addDeleteButton();
+  list.innerHTML = htmlPlayers;//Se llama a la variable list y se utiliza el metodo para indicar que se agregaran los objetos creados desde javaScript al html cuado corresponda
+  addDeleteButton();//Se llama a la función para darle funcionalidad al icono de borrar cada nombre ingresado
 };
   
 const addDeleteButton = (e) => {
