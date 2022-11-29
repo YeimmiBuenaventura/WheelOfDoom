37d17c614d0be482de6f3a51a285386638bc021a
Karen Andrea Niño Angulo <107644961+Andrea-25-11@users.noreply.github.com>
2022-11-29 11:25:39 -0500

    Funcionalidad con JS juego.html

--- funcionalidad.js
+++ funcionalidad.js
@@ -6,9 +6,18 @@
 //--------------------JAVASCRIPT LISTA.HTML--------------------
 
 // --------------------JAVASCRIPT JUEGO.HTML--------------------
-var botonDisparo = document.getElementById("disparar")
+let botonDisparo = document.getElementById("disparar")
+let canvasVictima = document.getElementById("victima")
+let canvasKillerCel = document.getElementById("killerCel")
+let canvaKiller = document.getElementById("killer")
 
 botonDisparo.addEventListener("click", iniciar);
+
+function iniciar (){
+    canvasKillerCel.classList.add("killerDisparandoCel")
+    canvasVictima.classList.add("muerto")
+    canvasVictima.classList.add("killerDisparando")
+}
 
 //CÓDIGO ANIMACIÓN MUÑEQUITO
 //FINALIZACIÓN ANIMACIÓN MUÑEQUITO
